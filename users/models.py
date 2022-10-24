54cdd99f31f4cc6f647dea712d57d8a95a9c0480
begievsanjar <begiyevsanjar@gmail.com>
2022-10-24 20:02:19 +0500

    1-commit

---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserModel(AbstractUser):
+    user_image = models.ImageField(upload_to='users/')
+    phone = models.CharField(max_length=13)
